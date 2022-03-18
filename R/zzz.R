9493d8fb90aa93a0b80fc9570899b481a2be3bcf
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-03-18 12:17:34 -0400

    Add caching
    Add memory based caching on package load

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname) {
+  if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
+    cm <- cachem::cache_mem(max_size = 512 * 1024^2, evict = "lru")
+    get_stats <<- memoise::memoise(get_stats, cache = cm)
+    get_aux <<- memoise::memoise(get_aux, cache = cm)
+    message("Info: Session based caching is enabled.")
+  }
+}
