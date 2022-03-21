b61eae599296ebb8946b068d677a7025aff33e2f
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-03-21 04:48:47 -0400

    Use packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,6 @@
     cm <- cachem::cache_mem(max_size = 512 * 1024^2, evict = "lru")
     get_stats <<- memoise::memoise(get_stats, cache = cm)
     get_aux <<- memoise::memoise(get_aux, cache = cm)
-    message("Info: Session based caching is enabled.")
+    packageStartupMessage("Info: Session based caching is enabled.")
   }
 }
