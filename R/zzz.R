d849c7a128f995ec135fb97dd5f526633f06ba1f
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-03-25 06:22:32 -0400

    Add get_wb() to on load caching

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
   if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
     cm <- cachem::cache_mem(max_size = 512 * 1024^2, evict = "lru")
     get_stats <<- memoise::memoise(get_stats, cache = cm)
+    get_wb <<- memoise::memoise(get_wb, cache = cm)
     get_aux <<- memoise::memoise(get_aux, cache = cm)
     packageStartupMessage("Info: Session based caching is enabled.")
   }
