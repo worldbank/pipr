aa85f33ef23825ec1b4f9ba285896c7797f12f1f
R.Andres Castaneda <acastanedaa@worldbank.org>
2023-05-04 18:53:52 -0400

    remove unnecesary tests

--- tests/testthat/test-caching.R
+++ tests/testthat/test-caching.R
@@ -41,20 +41,3 @@ test_that("Caching is enabled by default", {
 
   r$kill()
 })
-
-skip("can't not be disabled right not")
-test_that("Caching can be disabled", {
-  skip_on_cran()
-  # Setup external R session
-  r <- callr::r_session$new(options = callr::r_session_options(user_profile = FALSE))
-  r$run(function() Sys.setenv("PIPR_DISABLE_CACHING" = "TRUE"))
-  r$run(function() library(pipr))
-  # Check that main functions are NOT cached
-  tmp <- r$run(function() memoise::is.memoised(get_stats))
-  expect_false(tmp)
-  tmp <- r$run(function() memoise::is.memoised(get_wb))
-  expect_false(tmp)
-  tmp <- r$run(function() memoise::is.memoised(get_aux))
-  expect_false(tmp)
-  r$kill()
-})
