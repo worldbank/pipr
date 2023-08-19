d98d6bb9944b731b727dfc675b86b3b34aee8e9b
Ronak Shah <shahronak47@yahoo.in>
2023-08-19 10:07:55 +0800

    set default options on Load

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
+pipr_default_options <- list(
+  pipr.version  = NULL,
+  pipr.ppp_version  = NULL,
+  pipr.release_version = NULL,
+  pipr.table  = NULL
+)
 
 .onLoad <- function(libname, pkgname) {
   if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
@@ -11,9 +17,12 @@
     get_aux <<- memoise::memoise(get_aux, cache = cm)
     get_versions <<- memoise::memoise(get_versions, cache = cm)
   }
+  op    <- options()
+  toset <- !(names(pipr_default_options) %in% names(op))
+  if (any(toset)) options(pipr_default_options[toset])
 
   options(cli.ignore_unknown_rstudio_theme = TRUE)
-
+  invisible()
 }
 
 .onAttach <- function(libname, pkgname) {
