8eff3da17207973f4b39e8bdc7051ff5b82a1fee
tonyfujs <tony.fujs@gmail.com>
2023-09-01 18:35:54 +0200

    disable ad-hoc caching (to be replaced with httr2 cache mechanism)

--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,23 @@
 
-.onLoad <- function(libname, pkgname) {
-  if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
-    # d <- rappdirs::user_cache_dir("pipr")
-    # cm <- cachem::cache_disk(d,
-    #                          evict = "lru",
-    #                          max_size = 512 * 1024^2)
-    cm <- cachem::cache_mem(max_size = 512 * 1024^2, evict = "lru")
-    get_stats <<- memoise::memoise(get_stats, cache = cm)
-    get_wb <<- memoise::memoise(get_wb, cache = cm)
-    get_aux <<- memoise::memoise(get_aux, cache = cm)
-    get_versions <<- memoise::memoise(get_versions, cache = cm)
-  }
-
-  options(cli.ignore_unknown_rstudio_theme = TRUE)
-
-}
-
-.onAttach <- function(libname, pkgname) {
-  if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
-    packageStartupMessage("Info: Session based caching is enabled.")
-  }
-}
+# .onLoad <- function(libname, pkgname) {
+#   if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
+#     # d <- rappdirs::user_cache_dir("pipr")
+#     # cm <- cachem::cache_disk(d,
+#     #                          evict = "lru",
+#     #                          max_size = 512 * 1024^2)
+#     cm <- cachem::cache_mem(max_size = 512 * 1024^2, evict = "lru")
+#     get_stats <<- memoise::memoise(get_stats, cache = cm)
+#     get_wb <<- memoise::memoise(get_wb, cache = cm)
+#     get_aux <<- memoise::memoise(get_aux, cache = cm)
+#     get_versions <<- memoise::memoise(get_versions, cache = cm)
+#   }
+#
+#   options(cli.ignore_unknown_rstudio_theme = TRUE)
+#
+# }
+#
+# .onAttach <- function(libname, pkgname) {
+#   if (!Sys.getenv("PIPR_DISABLE_CACHING") == "TRUE") {
+#     packageStartupMessage("Info: Session based caching is enabled.")
+#   }
+# }
