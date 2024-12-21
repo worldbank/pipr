dd1006f5afcefa5c2f9c60cb6338019508108118
giorgiacek <giorgivola@gmail.com>
2024-12-21 13:59:44 -0500

    different version of test

--- local_pipr_test.r
+++ local_pipr_test.r
@@ -1,7 +1,7 @@
 library(devtools)
 load_all()
 
-
+# Version 1
 server = "http://127.0.0.1:8080"
 api_version = "v1"
 endpoint = "pip"
@@ -15,14 +15,24 @@ req <- httr2::request(server) |>
                    debug = TRUE) |>
   httr2::req_verbose()
 
-  #httr2::req_user_agent(pipr_user_agent) |>
-  #httr2::req_error(body = parse_error_body) |>
-  #httr2::req_retry(
-  #  is_transient = pip_is_transient,
-  #  after = retry_after,
-  #  max_seconds = 60
-  #) |>
-  #httr2::req_verbose()
+# Version 2
+req <- httr2::request("http://127.0.0.1:8080/api/v1/pip?country=COL&year=all&povline=2.15&fill_gaps=false&additional_ind=false&ppp_version=2017")
+
+req <- httr2::request("http://127.0.0.1:8080/api/v1/pip") |>
+  httr2::req_url_query(country = "COL", year = "all",
+                       povline = 2.15, fill_gaps = "false",
+                       additional_ind = "false", ppp_version = 2017) |>
+  httr2::req_cache(tools::R_user_dir("pipr", which = "cache"),
+                   use_on_error = TRUE,
+                   debug = TRUE) |>
+  httr2::req_user_agent(pipr_user_agent) |>
+  httr2::req_error(body = parse_error_body) |>
+  httr2::req_retry(
+    is_transient = pip_is_transient,
+    after = retry_after,
+    max_seconds = 60
+  ) |>
+  httr2::req_verbose()
 
 # Perform request
 res <- req |>
