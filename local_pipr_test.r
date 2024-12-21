803661223ec8b18e38c48eea1483abfdaecd2488
giorgiacek <giorgivola@gmail.com>
2024-12-21 13:54:57 -0500

    small change to local test script

--- local_pipr_test.r
+++ local_pipr_test.r
@@ -13,14 +13,16 @@ req <- httr2::request(server) |>
   httr2::req_cache(tools::R_user_dir("pipr", which = "cache"),
                    use_on_error = TRUE,
                    debug = TRUE) |>
-  httr2::req_user_agent(pipr_user_agent) |>
-  httr2::req_error(body = parse_error_body) |>
-  httr2::req_retry(
-    is_transient = pip_is_transient,
-    after = retry_after,
-    max_seconds = 60
-  ) |>
   httr2::req_verbose()
+
+  #httr2::req_user_agent(pipr_user_agent) |>
+  #httr2::req_error(body = parse_error_body) |>
+  #httr2::req_retry(
+  #  is_transient = pip_is_transient,
+  #  after = retry_after,
+  #  max_seconds = 60
+  #) |>
+  #httr2::req_verbose()
 
 # Perform request
 res <- req |>
